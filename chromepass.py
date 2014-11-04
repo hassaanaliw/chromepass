e9451a8b2d196353e393d265482e37faa651eb1e
hassaanaliw <hassaanaliw@gmail.com>
2014-11-04 20:46:16 +0500

    Tue Nov  4 20:46:16 PKT 2014 Init

---
+++ chromepass.py
@@ -0,0 +1,16 @@
+from os import getenv
+import sqlite3
+import win32crypt
+
+appdata = getenv("APPDATA") 
+connection = sqlite3.connect(appdata + "\..\Local\Google\Chrome\User Data\Default\Login Data")
+cursor = connection.cursor()
+cursor.execute('SELECT action_url, username_value, password_value FROM logins')
+for information in cursor.fetchall():
+        #chrome encrypts the password with Windows WinCrypt.
+	#Fortunately Decrypting it is no big issue.
+        pass = win32crypt.CryptUnprotectData(information[2], None, None, None, 0)[1]
+	if pass:
+		print 'website_link ' + information[0]
+		print 'Username: ' + information[1]
+		print 'Password: ' + password
