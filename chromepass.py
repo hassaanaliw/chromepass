284f079c93e0b04741ae5600a52972c22fba8186
Hassaan Ali Wattoo <hassaanaliw@gmail.com>
2014-11-06 18:30:15 +0500

    Output to csv added.

--- chromepass.py
+++ chromepass.py
@@ -1,7 +1,8 @@
 from os import getenv
 import sqlite3
 import win32crypt
-
+csv_file = open("chromepass.csv",'wb')
+csv_file.write("link,username,password\n".encode('utf-8'))
 appdata = getenv("APPDATA") 
 if appdata[-7:] == "Roaming": #Some WINDOWS Installations point to Roaming.
 	appdata = appdata[:-8]
@@ -16,3 +17,6 @@ for information in cursor.fetchall():
 		print('website_link ' + information[0])
 		print('Username: ' + information[1])
 		print('Password: ' + str(password))
+		csv_file.write(('%s,%s,%s\n'%(information[0],information[1],password)).encode('utf-8'))
+
+csv_file.close()		
