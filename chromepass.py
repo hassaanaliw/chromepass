77a56881c21026b811315994730b2ff5e85bf22a
Abd El-Aziz Khattat <secur1ty.c0d3r@gmail.com>
2018-03-21 10:25:06 +0000

    Update chromepass.py
    DRY..

--- chromepass.py
+++ chromepass.py
@@ -54,65 +54,53 @@ def main():
             print("Mac OSX not supported.")
             sys.exit(0)
 
-        for information in value:
+        for origin_url, username, password in value:
             if os.name == 'nt':
                 password = win32crypt.CryptUnprotectData(
-                    information[2], None, None, None, 0)[1]
-                if password:
-                    info_list.append({
-                        'origin_url': information[0],
-                        'username': information[1],
-                        'password': str(password)
-                    })
-
-            elif os.name == 'posix':
+                    password, None, None, None, 0)[1]
+            
+            if password:
                 info_list.append({
-                    'origin_url': information[0],
-                    'username': information[1],
-                    'password': information[2]
+                    'origin_url': origin_url,
+                    'username': username,
+                    'password': str(password)
                 })
 
     except sqlite3.OperationalError as e:
         e = str(e)
         if (e == 'database is locked'):
             print('[!] Make sure Google Chrome is not running in the background')
-            sys.exit(0)
         elif (e == 'no such table: logins'):
             print('[!] Something wrong with the database name')
-            sys.exit(0)
         elif (e == 'unable to open database file'):
             print('[!] Something wrong with the database path')
-            sys.exit(0)
         else:
             print(e)
-            sys.exit(0)
+        sys.exit(0)
 
     return info_list
 
 
 def getpath():
+
     if os.name == "nt":
         # This is the Windows Path
         PathName = os.getenv('localappdata') + \
             '\\Google\\Chrome\\User Data\\Default\\'
-        if (os.path.isdir(PathName) == False):
-            print('[!] Chrome Doesn\'t exists')
-            sys.exit(0)
-    elif ((os.name == "posix") and (sys.platform == "darwin")):
-        # This is the OS X Path
-        PathName = os.getenv(
-            'HOME') + "/Library/Application Support/Google/Chrome/Default/"
-        if (os.path.isdir(PathName) == False):
-            print('[!] Chrome Doesn\'t exists')
-            sys.exit(0)
-    elif (os.name == "posix"):
-        # This is the Linux Path
-        PathName = os.getenv('HOME') + '/.config/google-chrome/Default/'
-        if (os.path.isdir(PathName) == False):
-            print('[!] Chrome Doesn\'t exists')
-            sys.exit(0)
-
-    return PathName
+    elif os.name == "posix":
+        PathName = os.getenv('HOME')
+        if sys.platform == "darwin"
+            # This is the OS X Path
+            PathName += '/Library/Application Support/Google/Chrome/Default/'
+        else:
+            # This is the Linux Path
+            PathName += '/.config/google-chrome/Default/'
+    try:
+        assert os.path.isdir(PathName)
+        return PathName
+    except AssertionError:
+        print('[!] Chrome Doesn\'t exists')
+        sys.exit(0)
 
 
 def output_csv(info):
